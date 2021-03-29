728395f703e3751f3db18f8fd0a08c8c5d35de79
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-03-28 21:05:57 -0300

    Renamed files

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
-import './directory.component.scss';
+import './directory.styles.scss';
 
 export default class Directory extends React.Component {
     constructor() {
@@ -47,13 +47,8 @@ export default class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({ title, id, imageUrl, size }) => (
-                    <MenuItem
-                        key={id}
-                        title={title}
-                        imageUrl={imageUrl}
-                        size={size}
-                    />
+                {this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))}
             </div>
         );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import './menu-item.component.scss';
+import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history }) => {
     return (
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} onClick={() => history.push()}>
             <div
                 className="background-image"
                 style={{
                     backgroundImage: `url(${imageUrl})`,
                 }}
-            ></div>
+            />
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
