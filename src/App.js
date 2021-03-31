3710cf93913e95dfb79f58035e6bdabb81693e76
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-03-31 08:59:21 -0300

    Redirect if user is sign in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -7,7 +7,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import SignInAndSignOut from './pages/sing-in-sign-out/sing-in-sign-out.component';
+import SignInAndSignOut from './pages/sign-in-sign-out/sign-in-sign-out.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 
@@ -28,7 +28,7 @@ class App extends React.Component {
                     });
                 });
             }
-            
+
             setCurrentUser(userAuth);
         });
     }
@@ -44,15 +44,32 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage} />
                     <Route path='/shop' component={ShopPage} />
-                    <Route path='/signin' component={SignInAndSignOut} />
+                    <Route
+                        exact
+                        path='/signin'
+                        render={() =>
+                            this.props.currentUser ? (
+                                <Redirect to='/' />
+                            ) : (
+                                <SignInAndSignOut />
+                            )
+                        }
+                    />
                 </Switch>
             </div>
         );
     }
 }
 
-const mapDIspatchToProps = (dispatch) => ({
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser,
+});
+
+const mapDispatchToProps = (dispatch) => ({
     setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDIspatchToProps)(App);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
