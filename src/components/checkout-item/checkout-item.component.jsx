5bf343e822700c69a491768193f424863840fc02
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-03-31 17:43:10 -0300

    Added increase and decrease item to checkout item

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-import './checkout-item.styles.scss'
+import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem }) => {
+import {
+    addItem,
+    clearItemFromCart,
+    removeItem,
+} from './../../redux/cart/cart.actions';
+
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const { id, imageUrl, name, price, quantity } = cartItem;
 
     return (
@@ -11,11 +18,27 @@ const CheckoutItem = ({ cartItem }) => {
                 <img src={imageUrl} alt='item' />
             </div>
             <span className='name'>{name}</span>
-            <span className='quantity'>{quantity}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={() => removeItem(cartItem)}>
+                    &#10094;
+                </div>
+                <span className='value'>{quantity}</span>
+                <div className='arrow' onClick={() => addItem(cartItem)}>
+                    &#10095;
+                </div>
+            </span>
             <span className='price'>{price}</span>
-            <div className='remove-button'>&#10005;</div>
+            <div className='remove-button' onClick={() => clearItem(cartItem)}>
+                &#10005;
+            </div>
         </div>
     );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+    clearItem: (item) => dispatch(clearItemFromCart(item)),
+    addItem: (item) => dispatch(addItem(item)),
+    removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
