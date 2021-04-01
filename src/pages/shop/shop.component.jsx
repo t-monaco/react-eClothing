9a94f475c5ccf167ec9b17202167d22f39d6b052
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-04-01 13:51:56 -0300

    Removed collections from shopPage and created a separate component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,13 @@
-import React, { Component } from 'react'
-import CollectionPreview from '../../components/preview-collection/collection-preview.component'
+import React from 'react';
 
-import { SHOP_DATA } from './shop.data'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-export default class ShopPage extends Component {
-    constructor(props) {
-        super(props)
+import './shop.styles.scss';
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+const ShopPage = () => (
+    <div className='shop-page'>
+        <CollectionsOverview />
+    </div>
+);
 
-    render() {
-        const {collections} = this.state
-        return (
-            <div className='shop=page'>
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
-
-            </div>
-        )
-    }
-}
+export default ShopPage;
