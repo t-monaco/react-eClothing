fbefe028829f240dc516d0c71c480caedaffbf37
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-04-06 09:12:14 -0300

    Changed to styled-components

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,20 +1,68 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
 
-import './form-input.styles.scss';
-
-const FormInput = ({ handleChange, label, ...otherProps }) => (
-    <div className='group'>
-        <input className='form-input' onChange={handleChange} {...otherProps} />
+const FormInput = ({ handleChange, label, ...props }) => (
+    <GroupContainer>
+        <FormInputContainer onChange={handleChange} {...props} />
         {label ? (
-            <label
-                className={`${
-                    otherProps.value.length ? 'shrink' : ''
-                } form-input-label`}
-            >
+            <FormInputLabel className={props.value.length ? 'shrink' : ''}>
                 {label}
-            </label>
+            </FormInputLabel>
         ) : null}
-    </div>
+    </GroupContainer>
 );
 
 export default FormInput;
+
+// *** STYLES ***
+
+const subColor = 'grey';
+const mainColor = 'black';
+
+const shrinkLabelStyles = css`
+    top: -14px;
+    font-size: 12px;
+    color: ${mainColor};
+`;
+
+const GroupContainer = styled.div`
+    position: relative;
+    margin: 45px 0;
+    input[type='password'] {
+        letter-spacing: 0.3em;
+    }
+`;
+
+const FormInputContainer = styled.input`
+    background: none;
+    background-color: white;
+    color: ${subColor};
+    font-size: 18px;
+    padding: 10px 10px 10px 5px;
+    display: block;
+    width: 100%;
+    border: none;
+    border-radius: 0;
+    border-bottom: 1px solid ${subColor};
+    margin: 25px 0;
+    &:focus {
+        outline: none;
+    }
+    &:focus ~ label {
+        ${shrinkLabelStyles}
+    }
+`;
+
+const FormInputLabel = styled.label`
+    color: ${subColor};
+    font-size: 16px;
+    font-weight: normal;
+    position: absolute;
+    pointer-events: none;
+    left: 5px;
+    top: 10px;
+    transition: 300ms ease all;
+    &.shrink {
+        ${shrinkLabelStyles}
+    }
+`;
