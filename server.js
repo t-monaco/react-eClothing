bd300ed8e18370e23093c8cd0613bf5c03ced3b8
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-04-16 10:30:12 -0300

    Missing compression call

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(compression);
+app.use(compression());
 
 // TODO: try express.json() instead of bodyParser
 app.use(bodyParser.json());
