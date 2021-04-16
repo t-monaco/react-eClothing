7cf2b29fa18014fdda05536d814d1c98689bcb5c
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-04-16 09:25:12 -0300

    Added compression

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'produciton') require('dotenv').config();
 
@@ -9,6 +10,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(compression);
 
 // TODO: try express.json() instead of bodyParser
 app.use(bodyParser.json());
