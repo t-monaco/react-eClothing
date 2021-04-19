8ba9dc53d528118ed5044de32387de0778a5219e
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-04-19 13:33:25 -0300

    Updated collection name

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -15,7 +15,7 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
 
-    const userRef = firestore.doc(`user/${userAuth.uid}`);
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
     const snapshot = await userRef.get();
 
